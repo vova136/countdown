28140956decf8244f6f7eff5518f75a0835a0795
vova136 <vovah136@gmail.com>
2017-06-05 18:31:45 +0300

    add callback and stop timer

--- js/jquery.countdown.js
+++ js/jquery.countdown.js
@@ -1,61 +1,73 @@
 (function ($) {
     jQuery.fn.countDown = function (options) {
         var options = $.extend({
-                endDate: '2017-06-10'
+                endDate: new Date().setSeconds(+new Date().getSeconds() + 10),
+                format: 'DD/MM/YYYY HH:mm:ss',
+                callBack: function () {
+                    alert('time is over');
+                }
             },
             options);
-        console.log(options.endDate);
 
         return this.each(function () {
             var $this = $(this);
 
+            $('<div class="box" id="days"></div>').appendTo($this);
             $('<div class="box" id="hours"></div>').appendTo($this);
             $('<div class="box" id="minutes"></div>').appendTo($this);
             $('<div class="box" id="seconds"></div>').appendTo($this);
 
-            function timer(){
+
+
+
+var timeSegment;
+
+            function timer() {
                 var currentdate = new Date();
                 var endDate = new Date(options.endDate);
 
-                var timeSegment = endDate.getTime() - currentdate.getTime();
+                timeSegment = endDate.getTime() - currentdate.getTime();
 
 
                 var timeSegmentInSeconds = timeSegment / 1000;
 
+                var timeSegmentInDays = timeSegmentInSeconds / 86400;
+                timeSegmentInDays = Math.floor(timeSegmentInDays);
+                timeSegmentInSeconds = timeSegmentInSeconds - timeSegmentInDays * 86400;
+
+
                 var timeSegmentInHours = timeSegmentInSeconds / 3600;
                 timeSegmentInHours = Math.floor(timeSegmentInHours);
                 timeSegmentInSeconds = timeSegmentInSeconds - timeSegmentInHours * 3600;
+                if (timeSegmentInHours < 10) {
+                    timeSegmentInHours = "0" + timeSegmentInHours
+                }
 
                 var timeSegmentInMinutes = timeSegmentInSeconds / 60;
                 timeSegmentInMinutes = Math.floor(timeSegmentInMinutes);
                 timeSegmentInSeconds = timeSegmentInSeconds - timeSegmentInMinutes * 60;
+                if (timeSegmentInMinutes < 10) {
+                    timeSegmentInMinutes = "0" + timeSegmentInMinutes
+                }
 
                 timeSegmentInSeconds = Math.floor(timeSegmentInSeconds);
+                if (timeSegmentInSeconds < 10) {
+                    timeSegmentInSeconds = "0" + timeSegmentInSeconds
+                }
 
+                $('#days').html(timeSegmentInDays);
                 $('#hours').html(timeSegmentInHours);
                 $('#minutes').html(timeSegmentInMinutes);
                 $('#seconds').html(timeSegmentInSeconds);
-            }
                 
-            setInterval(timer, 1000);
+                if(timeSegment < 1000){
+                    clearInterval(interval);
+                    options.callBack(this);
 
-
-
-
-
-
-
-
-
-//            console.log(currentdate.getTime());
-//            console.log(endDate.getTime());
-//            console.log(timeSegment);
-//            console.log(timeSegmentInHours);
-//            console.log(timeSegmentInMinutes);
-//            console.log(timeSegmentInSeconds);
+                }
+            }
+            var interval=setInterval(timer,1000);
 
         });
-
-
     };
 })(jQuery);
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     $('#countdown').countDown({
-        endDate: '2017-07-11'
+//        endDate: '2017-06-05 17:10'
+//        format: 'yyyy-MM-dd HH:mm:ss' 
+        callBack: function(){
+            $('<div class="message">Time is over</div>').appendTo('#countdown');
+        }
     });
-    
 });
